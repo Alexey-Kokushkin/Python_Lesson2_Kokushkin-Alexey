a7d4332e5de654b409d93d3a9f06d18e1900d433
Alexey-Kokushkin <kokushkin_83@mail.ru>
2022-05-28 14:23:36 +0900

    Lesson_2 addde task_2

--- Lesson_2.py
+++ Lesson_2.py
@@ -1,6 +1,34 @@
 #Найти сумму чисел списка стоящих на нечетной позиции
 #Пример:[1,2,3,4] -> 4
 
+#def Value():
+
+    #import random
+ 
+    #Value = random.randrange(10)  # значение от 0 до 10
+    #return Value
+
+#numbers=[0,0,0,0,0,0]
+#i=0
+#while i<len(numbers):
+    #numbers[i]=Value()
+    #i+=1
+#print(numbers)
+
+#i=0
+#Sum=0
+#while i<len(numbers):
+    #Sum+=numbers[i]
+    #i+=2
+#print("Сумма чисел из списка, стоящих на нечетных позициях =", Sum)
+
+
+# Найти произведение пар чисел в списке. Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+# Пример: [2, 3, 4, 5, 6] => [12, 15, 16]; [2, 3, 5, 6] => [12, 15]
+
+from operator import le
+
+
 def Value():
 
     import random
@@ -16,8 +44,8 @@ while i<len(numbers):
 print(numbers)
 
 i=0
-Sum=0
-while i<len(numbers):
-    Sum+=numbers[i]
-    i+=2
-print("Сумма чисел из списка, стоящих на нечетных позициях =", Sum)
+Multiplication=0
+while i<len(numbers)/2:
+    Multiplication=numbers[i]*numbers[len(numbers)-i-1]
+    i+=1
+    print(Multiplication)
